83a0fc93d7df07ab5b06d28fed20500d89c19a42
Matthew Delisle <matthewdelisle@MatthewislesMBP.home>
2017-12-01 16:27:47 -0500

    numberDrills

---
+++ index.js
@@ -0,0 +1,36 @@
+//drill 1//
+
+function computeArea(width, height) {
+
+	return width * height;
+
+}
+
+//drill 2//
+
+function celsToFahr(celsTemp) {
+
+	return celsTemp * 1.8 + 32;
+
+}
+
+function fahrToCels(fahrTemp) {
+
+	return (fahrTemp - 32) / 1.8;
+
+}
+
+//drill 3//
+
+function isDivisible(divisee, divisor) {
+
+if( divisee % divisor === 0) {
+divisee / divisor;
+
+	return true;
+}
+
+	else {
+	return false;
+}
+}
